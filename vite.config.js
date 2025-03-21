827625adccc9ea1441c9f340bfe7ad68a95f86b9
Then Drianus <thendrianus@outlook.com>
2025-03-21 11:12:09 +0000

    chore: update config to run on replit

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,33 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { fileURLToPath } from 'url';
-import path from 'path';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { fileURLToPath } from "url";
+import path from "path";
 import sveltePreprocess from "svelte-preprocess";
-
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [svelte({
-    preprocess: sveltePreprocess(),
-  })],
+  plugins: [
+    svelte({
+      preprocess: sveltePreprocess(),
+    }),
+  ],
   server: {
+    allowedHosts: [".replit.dev", "localhost", "127.0.0.1"],
     port: 3000,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
+      "/api": {
+        target: "http://localhost:8080",
         changeOrigin: true,
-        secure: false
-      }
-    }
+        secure: false,
+      },
+    },
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
